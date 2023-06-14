d50bd61dafb1af1ec6fe77cb16140f967380b8c3
Vaidas <mano@mailas.lt>
2023-06-14 14:28:35 +0300

    initial commit

---
+++ myfunctions.js
@@ -0,0 +1,9 @@
+function showAlert(){
+    window.alert("Veikia isorinis skriptas");
+}
+
+function outputToConsole(str){
+    console.debug("outputToConsole got call");
+    console.info(str);
+    
+}
